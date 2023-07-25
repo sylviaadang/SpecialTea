a17c75889115ad5d79962f8730868d74e9efed76
Your Name <email@example.com>
2023-07-25 13:31:58 -0700

    updated

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -2,6 +2,12 @@ const Cart = require('../models/cart.model')
 
 module.exports = {
 
+    all: (req, res) => {
+        Cart.find()
+            .then(cart => res.json(cart))
+            .catch (cart => res.json(err))
+    },
+
     update: (req, res) => {
         Cart.findOneAndUpdate({_id: req.params.id}, {runValidators: true, new: true})
             .then(cart => res.json(cart))
