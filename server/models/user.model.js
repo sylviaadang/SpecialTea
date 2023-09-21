3b52dbccfc256017831330c2afe1698de731423f
Your Name <email@example.com>
2023-09-21 16:58:15 -0700

    updated

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     first_name: {
@@ -29,6 +29,25 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+UserSchema.virtual('confirmPassword')
+    .get( () => this._confirmPassword)
+    .set( value => this._confirmPassword = value);
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this._confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password');
+    }
+    next();
+});
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
 
 const User = mongoose.model('User', UserSchema)
 module.exports = User
